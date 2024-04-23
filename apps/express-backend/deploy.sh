7df1dbe166e53746b559bb126c7ad3cefbef0479
Yash Prasad <64632275+yashprasad234@users.noreply.github.com>
2024-04-23 13:02:59 +0530

    added scripts and deploy.sh

---
+++ apps/express-backend/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.7.3
+
+cd /home/ubuntu/monorepo-deployment
+git pull origin master
+yarn install
+yarn build
+pm2 stop express
+pm2 start npm --name "express" -- run "start:express" 
---
+++ apps/next-client/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.7.3
+
+cd /home/ubuntu/monorepo-deployment
+git pull origin master
+yarn install
+yarn build
+pm2 stop next
+pm2 start npm --name "next" -- run "start:next" 
